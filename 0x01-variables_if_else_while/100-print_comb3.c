65c7e7873e3ae949ad5e2259022a3b62f7cb1c14
nriama marydonald <mayd_beauty@yahoo.com>
2022-06-05 05:09:02 -0700

    my name

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two digits,
- *	in ascending order, separated by a space.
+ * main - Prints all possible combinations of two different digits,
+ *	in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
