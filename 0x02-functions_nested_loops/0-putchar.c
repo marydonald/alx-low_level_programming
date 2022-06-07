f65482609aa748036fe8f85f0014da498c3541ba
nriama marydonald <mayd_beauty@yahoo.com>
2022-06-07 15:16:48 -0700

    afa

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,4 @@
-/*
- * File: 0-putchar.c
- * Auth: Bardi Gideon
- *
- */
-
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Prints "_putchar" followed by a new line.
@@ -13,8 +7,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
 
