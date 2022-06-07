d2f76df5f261425bdb0e4e036814663092f0e50d
nriama marydonald <mayd_beauty@yahoo.com>
2022-06-07 16:33:38 -0700

    afa

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,13 @@
 #ifndef main_h
 #define main_h
 
-int_putchar(char);
+int _putchar(char);
 void print_alphabet(void);
-void
-print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
