da01e86383b1ec586746605a5c4d8c694ff7d45f
nriama marydonald <mayd_beauty@yahoo.com>
2022-06-07 13:58:42 -0700

    mmmmm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  * Return: Always: 0.
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char a;
 
-	for(a= 'a'; a <= 'z'; a++)
+	for(a = 'a'; a <= 'z'; a++)
 	{
 		_putchar('a');
 	}
