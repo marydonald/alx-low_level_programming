bfcd7ffc98469c110eb6d167926808d4782d2705
nriama marydonald <mayd_beauty@yahoo.com>
2022-06-16 10:57:21 -0700

    my dreams

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 	
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index]
-			
-			return (dest);
+		dest[dest_len++] = src[index];
+	
+	return (dest);
 }
